6b2f9362558554495e5f40ec30e585dbdec7f1e5
sergey-fedunets <serik.fedunets@gmail.com>
2014-03-05 12:10:49 +0200

    margin QBChangePasswordTask.

--- Q-municate/src/main/java/com/quickblox/qmunicate/qb/QBChangePasswordTask.java
+++ Q-municate/src/main/java/com/quickblox/qmunicate/qb/QBChangePasswordTask.java
@@ -1,7 +1,39 @@
 package com.quickblox.qmunicate.qb;
 
-/**
- * Created by Sergey Fedunets on 05.03.14.
- */
-public class QBChangePasswordTask {
+import android.app.Activity;
+
+import com.quickblox.module.users.QBUsers;
+import com.quickblox.module.users.model.QBUser;
+import com.quickblox.qmunicate.App;
+import com.quickblox.qmunicate.R;
+import com.quickblox.qmunicate.core.concurrency.BaseProgressTask;
+import com.quickblox.qmunicate.ui.utils.DialogUtils;
+
+public class QBChangePasswordTask extends BaseProgressTask<Object, Void, Void> {
+
+    public QBChangePasswordTask(Activity activity) {
+        super(activity);
+    }
+
+    @Override
+    public Void performInBackground(Object... params) throws Exception {
+        QBUser user = (QBUser) params[0];
+
+        String password = user.getPassword();
+
+        user = QBUsers.updateUser(user);
+        user.setPassword(password);
+        App.getInstance().setUser(user);
+
+        return null;
+    }
+
+    @Override
+    public void onResult(Void aVoid) {
+        super.onResult(aVoid);
+        final Activity activity = activityRef.get();
+        if (isActivityAlive()) {
+            DialogUtils.show(activity, activity.getString(R.string.dlg_user_updated));
+        }
+    }
 }
