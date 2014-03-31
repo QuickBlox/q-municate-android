e694bcb1815baffe5cb67c7fb29b7675bda77021
sergey-fedunets <serik.fedunets@gmail.com>
2014-03-31 16:30:23 +0300

    first commit

---
+++ Q-municate/src/main/java/com/quickblox/qmunicate/caching/DatabaseWrapper.java
@@ -0,0 +1,7 @@
+package com.quickblox.qmunicate.caching;
+
+/**
+ * Created by Sergey Fedunets on 31.03.2014.
+ */
+public class DatabaseWrapper {
+}
---
+++ Q-municate/src/main/java/com/quickblox/qmunicate/caching/orm/FriendORM.java
@@ -0,0 +1,7 @@
+package com.quickblox.qmunicate.caching.orm;
+
+/**
+ * Created by Sergey Fedunets on 31.03.2014.
+ */
+public class FriendORM {
+}
