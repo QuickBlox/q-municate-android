d4bef178879b83c8ab50abdd157f522051ed892d
sergey-fedunets <serik.fedunets@gmail.com>
2014-05-19 15:59:26 +0300

    first commit

--- Q-municate/src/main/java/com/quickblox/qmunicate/ui/chats/PrivateChatMessagesAdapter.java
+++ Q-municate/src/main/java/com/quickblox/qmunicate/ui/chats/PrivateChatMessagesAdapter.java
@@ -10,7 +10,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.quickblox.qmunicate.R;
-import com.quickblox.qmunicate.caching.DatabaseManager;
 import com.quickblox.qmunicate.caching.tables.ChatMessagesTable;
 import com.quickblox.qmunicate.model.Friend;
 import com.quickblox.qmunicate.ui.base.BaseCursorAdapter;
@@ -29,13 +28,18 @@ public class PrivateChatMessagesAdapter extends BaseCursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = layoutInflater.inflate(R.layout.list_item_private_chat_message, null, true);
+        View view;
+        int senderId = cursor.getInt(cursor.getColumnIndex(ChatMessagesTable.Cols.SENDER_ID));
+        if (isOwnMessage(senderId)) {
+            view = layoutInflater.inflate(R.layout.list_item_private_chat_message_left, null, true);
+        } else {
+            view = layoutInflater.inflate(R.layout.list_item_private_chat_message_right, null, true);
+        }
 
         ViewHolder holder = new ViewHolder();
 
         holder.avatarImageView = (RoundedImageView) view.findViewById(R.id.avatar_imageview);
         holder.avatarImageView.setOval(true);
-        holder.nameTextView = (TextView) view.findViewById(R.id.name_textview);
         holder.messageTextView = (ChatTextView) view.findViewById(R.id.message_textview);
         holder.attachImageView = (ImageView) view.findViewById(R.id.attach_imageview);
         holder.timeTextView = (TextView) view.findViewById(R.id.time_textview);
@@ -46,10 +50,13 @@ public class PrivateChatMessagesAdapter extends BaseCursorAdapter {
         return view;
     }
 
+    private boolean isOwnMessage(int senderId) {
+        return senderId == currentUser.getId();
+    }
+
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         ViewHolder holder = (ViewHolder) view.getTag();
-        String senderName;
         String avatarUrl;
 
         String body = cursor.getString(cursor.getColumnIndex(ChatMessagesTable.Cols.BODY));
@@ -57,17 +64,13 @@ public class PrivateChatMessagesAdapter extends BaseCursorAdapter {
         int senderId = cursor.getInt(cursor.getColumnIndex(ChatMessagesTable.Cols.SENDER_ID));
         long time = cursor.getLong(cursor.getColumnIndex(ChatMessagesTable.Cols.TIME));
 
-        if(senderId == currentUser.getId()) {
-            senderName = currentUser.getFullName();
+        if (senderId == currentUser.getId()) {
             avatarUrl = getAvatarUrlForCurrentUser();
         } else {
-            Cursor senderCursor = DatabaseManager.getCursorFriendById(context, senderId);
-            Friend senderFriend = DatabaseManager.getFriendFromCursor(senderCursor);
-            senderName = senderFriend.getFullname();
             avatarUrl = getAvatarUrlForFriend(opponentFriend);
         }
 
-        if(!TextUtils.isEmpty(attachUrl)) {
+        if (!TextUtils.isEmpty(attachUrl)) {
             holder.messageTextView.setVisibility(View.GONE);
             holder.attachImageView.setVisibility(View.VISIBLE);
             displayAttachImage(attachUrl, holder.attachImageView, holder.progressBar);
@@ -76,7 +79,6 @@ public class PrivateChatMessagesAdapter extends BaseCursorAdapter {
             holder.attachImageView.setVisibility(View.GONE);
             holder.messageTextView.setText(body);
         }
-        holder.nameTextView.setText(senderName);
         holder.timeTextView.setText(DateUtils.longToMessageDate(time));
 
         displayAvatarImage(avatarUrl, holder.avatarImageView);
@@ -85,7 +87,6 @@ public class PrivateChatMessagesAdapter extends BaseCursorAdapter {
     private static class ViewHolder {
 
         RoundedImageView avatarImageView;
-        TextView nameTextView;
         ChatTextView messageTextView;
         ImageView attachImageView;
         TextView timeTextView;
