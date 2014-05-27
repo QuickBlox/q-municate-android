0288134c2ffa6865bcb850c0168869aa62be6b4b
sergey-fedunets <serik.fedunets@gmail.com>
2014-05-27 15:48:42 +0300

    Unread chats badge was added.

--- Q-municate/src/main/java/com/quickblox/qmunicate/ui/main/NavigationDrawerAdapter.java
+++ Q-municate/src/main/java/com/quickblox/qmunicate/ui/main/NavigationDrawerAdapter.java
@@ -7,10 +7,13 @@ import android.widget.TextView;
 import com.quickblox.qmunicate.R;
 import com.quickblox.qmunicate.ui.base.BaseActivity;
 import com.quickblox.qmunicate.ui.base.BaseListAdapter;
+import com.quickblox.qmunicate.utils.Consts;
 
 import java.util.List;
 
-public class NavigationDrawerAdapter extends BaseListAdapter<String> {
+public class NavigationDrawerAdapter extends BaseListAdapter<String> implements NavigationDrawerFragment.UpdateCountUnreadChatsDialogsListener {
+
+    private TextView counterUnreadChatsDialogs;
 
     public NavigationDrawerAdapter(BaseActivity activity, List<String> objects) {
         super(activity, objects);
@@ -20,14 +23,25 @@ public class NavigationDrawerAdapter extends BaseListAdapter<String> {
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
         final String data = getItem(position);
+        String chatItem = resources.getStringArray(
+                R.array.nvd_items_array)[MainActivity.ID_CHATS_LIST_FRAGMENT];
 
         if (convertView == null) {
             convertView = layoutInflater.inflate(R.layout.list_item_navigation_drawer, null);
             holder = new ViewHolder();
             holder.nameTextView = (TextView) convertView.findViewById(R.id.name_textview);
+            holder.unreadMessagesTextView = (TextView) convertView.findViewById(
+                    R.id.unread_messages_textview);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
+        }
+
+        if (data.equals(chatItem)) {
+            holder.unreadMessagesTextView.setVisibility(View.VISIBLE);
+            counterUnreadChatsDialogs = holder.unreadMessagesTextView;
+        } else {
+            holder.unreadMessagesTextView.setVisibility(View.GONE);
         }
 
         holder.nameTextView.setText(data);
@@ -35,8 +49,14 @@ public class NavigationDrawerAdapter extends BaseListAdapter<String> {
         return convertView;
     }
 
+    @Override
+    public void onUpdateCountUnreadChatsDialogs(int count) {
+        counterUnreadChatsDialogs.setText(count + Consts.EMPTY_STRING);
+    }
+
     private static class ViewHolder {
 
         TextView nameTextView;
+        TextView unreadMessagesTextView;
     }
 }
