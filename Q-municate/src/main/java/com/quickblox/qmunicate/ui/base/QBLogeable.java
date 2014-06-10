0545bb5be2cd5b832964973013ad6c454ce993ac
vfite <djoweider@rambler.ru>
2014-06-10 19:06:42 +0300

    Merge branch 'dev' of https://bitbucket.org/quickblox/q-municate-android into dev
    Conflicts:
    	Q-municate/src/main/java/com/quickblox/qmunicate/qb/commands/QBLoginChatCommand.java
    	Q-municate/src/main/java/com/quickblox/qmunicate/utils/ActivityLifecycleHandler.java

--- Q-municate/src/main/java/com/quickblox/qmunicate/ui/base/QBLogeable.java
+++ Q-municate/src/main/java/com/quickblox/qmunicate/ui/base/QBLogeable.java
@@ -1,9 +1,5 @@
 package com.quickblox.qmunicate.ui.base;
 
-
-/*
-    //This interface is used for logout action when Actvity is going to background
-*/
 public interface QBLogeable {
 
     public boolean isCanPerformLogoutInOnStop();
