0b5f6c34106bad1f11142c667316928e43702203
vfite <djoweider@rambler.ru>
2014-06-16 17:32:51 +0300

    added replying time to chat

--- Q-municate/src/main/java/com/quickblox/qmunicate/qb/helpers/QBChatHelper.java
+++ Q-municate/src/main/java/com/quickblox/qmunicate/qb/helpers/QBChatHelper.java
@@ -230,6 +230,7 @@ public class QBChatHelper extends BaseHelper implements QBPrivateChatManagerList
     public synchronized void login(QBUser user) throws XMPPException, IOException, SmackException {
         if (!QBChatService.isInitialized()) {
             QBChatService.init(context);
+            QBChatService.setDefaultPacketReplyTimeout(7 * 1000);
             chatService = QBChatService.getInstance();
         }
         if (!chatService.isLoggedIn() && user != null) {
