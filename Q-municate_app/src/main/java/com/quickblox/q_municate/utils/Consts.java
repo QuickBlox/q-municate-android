1503919bfb56b61c5de2e759069c4742fb6d6a40
evgeniybogatov <evgeniy.bogatov@quickblox.com>
2015-06-30 20:26:42 +0300

    Build 0.9.2.25062015. Set position of dialog  list by last clicked item (QMUN-889)

--- Q-municate_app/src/main/java/com/quickblox/q_municate/utils/Consts.java
+++ Q-municate_app/src/main/java/com/quickblox/q_municate/utils/Consts.java
@@ -18,7 +18,7 @@ public class Consts {
     // Universal Image Loader
     public static final DisplayImageOptions UIL_DEFAULT_DISPLAY_OPTIONS = new DisplayImageOptions.Builder()
             .imageScaleType(ImageScaleType.IN_SAMPLE_POWER_OF_2).bitmapConfig(Bitmap.Config.RGB_565)
-            .cacheOnDisc(true).cacheInMemory(true).build();
+            .cacheOnDisc(true).cacheInMemory(true).considerExifParams(true).build();
 
     public static final DisplayImageOptions UIL_USER_AVATAR_DISPLAY_OPTIONS = new DisplayImageOptions.Builder()
             .showImageOnLoading(R.drawable.placeholder_user).showImageForEmptyUri(R.drawable.placeholder_user)
