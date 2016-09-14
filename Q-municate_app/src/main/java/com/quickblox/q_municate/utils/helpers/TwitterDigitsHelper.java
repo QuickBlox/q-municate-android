04e2535ab9d6e82b6dab3225471e634f85ca4c8c
Tereha <tereha@Andreys-Mac-mini.local>
2016-09-14 18:22:25 +0300

    - fixed issue after adding new logic of initialization Crashlytics (bugs QMUN-1807, QMUN-1808)

--- Q-municate_app/src/main/java/com/quickblox/q_municate/utils/helpers/TwitterDigitsHelper.java
+++ Q-municate_app/src/main/java/com/quickblox/q_municate/utils/helpers/TwitterDigitsHelper.java
@@ -1,42 +1,21 @@
 package com.quickblox.q_municate.utils.helpers;
 
-import android.content.Context;
-
 import com.digits.sdk.android.AuthCallback;
 import com.digits.sdk.android.AuthConfig;
 import com.digits.sdk.android.Digits;
 import com.digits.sdk.android.DigitsOAuthSigning;
-import com.quickblox.q_municate.R;
-import com.quickblox.q_municate.utils.StringObfuscator;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterCore;
 
 import java.util.Map;
 
-import io.fabric.sdk.android.Fabric;
-
 public class TwitterDigitsHelper {
-
-    private final Context context;
 
     public static final String PROVIDER = "X-Auth-Service-Provider";
     public static final String CREDENTIALS = "X-Verify-Credentials-Authorization";
 
-    public TwitterDigitsHelper(Context context) {
-        this.context = context;
-        initTwitterDigits();
-    }
-
-    private void initTwitterDigits() {
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(
-                StringObfuscator.getTwitterConsumerKey(),
-                StringObfuscator.getTwitterConsumerSecret());
-
-        Fabric.with(context.getApplicationContext(),
-                new TwitterCore(authConfig),
-                new Digits.Builder()
-                        .withTheme(R.style.AppTheme).build());
+    public TwitterDigitsHelper() {
     }
 
     public void login(AuthCallback authCallback){
