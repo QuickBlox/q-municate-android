cb0cb48c567fb271fab32b749db7ed98d9fbf4e7
Roman Pronin <roman.pronin@injoit.com>
2017-02-06 18:27:06 +0200

    fix QMUN-1920 Can't login the app via email as several users

--- Q-municate_app/src/main/java/com/quickblox/q_municate/ui/activities/authorization/ServiceManager.java
+++ Q-municate_app/src/main/java/com/quickblox/q_municate/ui/activities/authorization/ServiceManager.java
@@ -5,9 +5,7 @@ import android.util.Log;
 
 import com.quickblox.auth.QBAuth;
 import com.quickblox.auth.session.QBSessionManager;
-import com.quickblox.core.exception.QBResponseException;
 import com.quickblox.q_municate.R;
-import com.quickblox.q_municate.ui.activities.authorization.BaseAuthActivity;
 import com.quickblox.q_municate.utils.helpers.FlurryAnalyticsHelper;
 import com.quickblox.q_municate.utils.helpers.GoogleAnalyticsHelper;
 import com.quickblox.q_municate_auth_service.QMAuthService;
@@ -20,6 +18,8 @@ import com.quickblox.q_municate_user_service.QMUserService;
 import com.quickblox.q_municate_user_service.cache.QMUserCache;
 import com.quickblox.q_municate_user_service.model.QMUser;
 import com.quickblox.users.model.QBUser;
+
+import java.util.concurrent.Callable;
 
 import rx.Observable;
 import rx.Observer;
@@ -41,7 +41,7 @@ public class ServiceManager {
     private QMUserService userService;
     private QMUserCache userCache;
 
-    public ServiceManager(BaseAuthActivity authActivity){
+    public ServiceManager(BaseAuthActivity authActivity) {
         this.authActivity = authActivity;
         authService = QMAuthService.getInstance();
         userService = QMUserService.getInstance();
@@ -50,79 +50,84 @@ public class ServiceManager {
 
     public void login(QBUser user) {
         final String userPassword = user.getPassword();
-        authService.login(user).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-        .map(new Func1<QBUser,QMUser>() {
-            @Override
-            public QMUser call(QBUser qbUser) {
-                QMUser result = QMUser.convert(qbUser);
-                userCache.createOrUpdate(result);
-                return result;
-            }
-        })
-        .subscribe(new Observer<QBUser>() {
-            @Override
-            public void onCompleted() {
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "onError" + e.getMessage());
-                authActivity.hideProgress();
-                parseExceptionMessage(e.getMessage());
-            }
-
-            @Override
-            public void onNext(QBUser qbUser) {
-                String password = userPassword;
-
-                if (!hasUserCustomData(qbUser)) {
-                    qbUser.setOldPassword(password);
-                    try {
-                        updateUser(qbUser);
-                    } catch (QBResponseException e) {
-                        e.printStackTrace();
+        authService.login(user)
+                .map(new Func1<QBUser, QMUser>() {
+                    @Override
+                    public QMUser call(QBUser qbUser) {
+                        QMUser result = QMUser.convert(qbUser);
+                        userCache.createOrUpdate(result);
+                        return result;
                     }
-                }
+                })
+                .flatMap(new Func1<QBUser, Observable<QBUser>>() {
+                    @Override
+                    public Observable<QBUser> call(QBUser qbUser) {
+                        return updateUserIfNeed(qbUser, userPassword);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<QBUser>() {
+                    @Override
+                    public void onCompleted() {
+                    }
 
-                String token = QBSessionManager.getInstance().getToken();
-                qbUser.setPassword(password);
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "onError" + e.getMessage());
+                        e.printStackTrace();
+                        authActivity.hideProgress();
+                        parseExceptionMessage(e.getMessage());
+                    }
 
-                saveOwnerUser(qbUser);
+                    @Override
+                    public void onNext(QBUser qbUser) {
 
-                AppSession.startSession(qbUser);
+                        String token = QBSessionManager.getInstance().getToken();
+                        qbUser.setPassword(userPassword);
+
+                        saveOwnerUser(qbUser);
+
+                        AppSession.startSession(qbUser);
 
 
-                authActivity.startMainActivity(qbUser);
+                        authActivity.startMainActivity(qbUser);
 
-                // send analytics data
-                GoogleAnalyticsHelper.pushAnalyticsData(authActivity, qbUser, "User Sign In");
-                FlurryAnalyticsHelper.pushAnalyticsData(authActivity);
-            }
-        });
+                        // send analytics data
+                        GoogleAnalyticsHelper.pushAnalyticsData(authActivity, qbUser, "User Sign In");
+                        FlurryAnalyticsHelper.pushAnalyticsData(authActivity);
+                    }
+                });
     }
 
-    private QBUser updateUser(QBUser inputUser) throws QBResponseException {
-        QBUser user;
+    private Observable<QBUser> updateUserIfNeed(final QBUser inputUser, final String password) {
 
-        String password = inputUser.getPassword();
+        return Observable.fromCallable(new Callable<QBUser>() {
+            @Override
+            public QBUser call() throws Exception {
+                QBUser user = inputUser;
 
-        UserCustomData userCustomDataNew = getUserCustomData(inputUser);
-        inputUser.setCustomData(Utils.customDataToString(userCustomDataNew));
+                if (!hasUserCustomData(inputUser)) {
+                    inputUser.setOldPassword(password);
 
-        inputUser.setPassword(null);
-        inputUser.setOldPassword(null);
+                    String password = inputUser.getPassword();
 
-        user = userService.updateUserSync(QMUser.convert(inputUser));
+                    UserCustomData userCustomDataNew = getUserCustomData(inputUser);
+                    inputUser.setCustomData(Utils.customDataToString(userCustomDataNew));
 
+                    inputUser.setPassword(null);
+                    inputUser.setOldPassword(null);
+                    user = userService.updateUserSync(QMUser.convert(inputUser));
 
-        if (LoginType.EMAIL.equals(AppSession.getSession().getLoginType())) {
-            user.setPassword(password);
-        } else {
-            user.setPassword(QBAuth.getSession().perform().getToken());
-        }
-
-        return user;
+                    if (LoginType.EMAIL.equals(AppSession.getSession().getLoginType())) {
+                        user.setPassword(password);
+                    } else {
+                        user.setPassword(QBAuth.getSession().perform().getToken());
+                    }
+                }
+                return user;
+            }
+        });
     }
 
     private boolean hasUserCustomData(QBUser user) {
